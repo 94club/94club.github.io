aa7dc58771c57cd08982f72637202ec6e816e649
shika233 <1692708036@qq.com>
2018-06-22 21:57:00 +0800

    安装依赖

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,34 @@
 <template>
-  <div id="app">
-    <router-view/>
+  <div>
+    <transition name="router-fade" mode="out-in">
+      <keep-alive>
+        <router-view v-if="$route.meta.keepAlive"/>
+      </keep-alive>
+    </transition>
+    <transition name="router-fade" mode="out-in">
+      <keep-alive>
+        <router-view v-if="!$route.meta.keepAlive"/>
+      </keep-alive>
+    </transition>
+    <svg-icon/>
   </div>
 </template>
 
 <script>
+import svgIcon from './components/common/svg'
 export default {
-  name: 'App'
+  components: {
+    svgIcon
+  }
 }
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+<style lang="scss">
+@import './style/common';
+.router-fade-enter-active, .router-fade-leave-active {
+  transition: opacity .3s;
+}
+.router-fade-enter, .router-fade-leave-active {
+  opacity: 0;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,15 +1,37 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
-import App from './App'
-import router from './router/router'
+import VueRouter from 'vue-router'
+import routes from './router/router'
+import store from './store/'
+import { routerMode } from './config/env'
+import './config/rem'
+import FastClick from 'fastclick'
 
+if ('addEventListener' in document) {
+  document.addEventListener('DOMContentLoaded', function () {
+    FastClick.attach(document.body)
+  }, false)
+}
 Vue.config.productionTip = false
-
+Vue.use(VueRouter)
+const router = new VueRouter({
+  routes,
+  mode: routerMode,
+  strict: process.env.NODE_ENV !== 'production',
+  scrollBehavior (to, from, savedPosition) {
+    if (savedPosition) {
+      return savedPosition
+    } else {
+      if (from.meta.keepAlive) {
+        from.meta.savedPosition = document.body.scrollTop
+      }
+      return { x: 0, y: to.meta.savedPosition || 0 }
+    }
+  }
+})
 /* eslint-disable no-new */
 new Vue({
-  el: '#app',
   router,
-  components: { App },
-  template: '<App/>'
-})
+  store
+}).$mount('#app')
