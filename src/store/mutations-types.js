feb9ac593c0ffa1c5786f4152cf8f418e70eec6b
alistar <2323504502@qq.com>
2018-10-27 20:56:40 +0800

    delete all

--- src/store/mutations-types.js
+++
@@ -1,8 +0,0 @@
-export const RECORD_USERINFO = 'RECORD_USERINFO'
-export const GET_USERINFO = 'GET_USERINFO'
-export const SAVE_GEOHASH = 'SAVE_GEOHASH'
-export const RECORD_ADDRESS = 'RECORD_ADDRESS'
-export const OUT_LOGIN = 'OUT_LOGIN'
-export const SAVE_AVANDER = 'SAVE_AVANDER'
-export const ORDER_SUCCESS = 'ORDER_SUCCESS'
-export const BUY_CART = 'BUY_CART'
